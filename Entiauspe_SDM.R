e76c7df126782c2302384a6abff65555cfec81e4
Omar M. Entiauspe-Neto <85691416+omarentiauspe@users.noreply.github.com>
2023-03-30 21:34:28 -0300

    Update Entiauspe_SDM.R

--- Entiauspe_SDM.R
+++ Entiauspe_SDM.R
@@ -151,6 +151,14 @@ layers <- c(1, 2, 3, 4, 5, 6, 7, 8,
             9, 10, 11, 12, 13, 14,
             15, 16, 17, 18, 19)
 
+writeRaster(
+  stack(envcrop[[layers]]),
+  paste0("bio", layers), 
+  bylayer = TRUE,
+  format = 'ascii',
+  overwrite = T
+)
+
 # Presence - absence map #
 
 
